f0aa9d355ef01a0e871f8e2dd5c307c15da0e63b
firante <misha.fedorovych@gmail.com>
2017-10-12 00:31:43 +0300

    change month by click to next/prev month day

--- src/app/body/calendar/td.calendar.ts
+++ src/app/body/calendar/td.calendar.ts
@@ -1,17 +1,33 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+import Calendar from '../../_services/calendar/calendar';
+
 @Component ({
-    selector: '[td-calendar]',
+
+selector: '[td-calendar]',
     templateUrl: './td.calendar.html',
     styleUrls: ['./td.calendar.css']
 })
 
 export class TdCalendar implements OnInit {
+  constructor(private calendar: Calendar) { }
   @Input() tdItem: any[];
 
-  ngOnInit() {  }
+  ngOnInit() { }
   
   getTdItem(): string {
     return this.tdItem;
+  }
+
+  dayClick(item): void {
+    if (item && item.day && !item.current) {
+      const day = parseInt(item.day, 10);
+      if (day > 15) {
+	this.calendar.currentMonth = this.calendar.currentMonth - 1;
+      } else {
+	this.calendar.currentMonth = this.calendar.currentMonth + 1;
+      }
+    }
+  
   }
 }
